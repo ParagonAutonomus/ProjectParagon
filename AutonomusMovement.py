d168d0fc6220b2631eebf7b1b0babe2aee17dbf1
EnnJayWithAGreyHat <njsvoboda04@gmail.com>
2024-04-12 18:31:45 -0700

    formatting

--- AutonomusMovement.py
+++ AutonomusMovement.py
@@ -189,7 +189,7 @@ def drawPoints(img, points):
     for point in points:
         cv2.circle(img, point, 5,(0,0,255), cv2.FILLED)
         cv2.circle(img, points[-1], 7,(0,255,255), cv2.FILLED)
-    cv2.putText(img, f'({(points[-1][0]-500)/100},{(points[-1][1]-500)/100})m', 
+        cv2.putText(img, f'({(points[-1][0]-500)/100},{(points[-1][1]-500)/100})m', 
                 (points[-1][0]+10,points[-1][1]+30),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),1)
     
 while True:
